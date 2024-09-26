bfdd6a637424fb394a7c5f0552ffc7f3ddfd70c7
David Sebold <d.seibold@icloud.com>
2024-09-26 13:13:25 -0700

    added header and footer to submit page

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,9 +1,15 @@
 import RecipeForm from "../components/RecipeForm";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function SubmitRecipePage() {
   return (
-    <div className="min-h-screen flex flex-col justify-center">
-      <RecipeForm />
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow flex flex-col justify-center">
+        <RecipeForm />
+      </main>
+      <Footer />
     </div>
   );
 }
