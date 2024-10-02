87ecd4a09491db7186a87702760442cafd8cf448
David Sebold <d.seibold@icloud.com>
2024-10-02 11:48:10 -0700

    Import route correction in recipes.js. All Postman tests successful

--- pages/api/recipes.js
+++ pages/api/recipes.js
@@ -1,6 +1,6 @@
-import dbConnect from "../../../lib/mongodb";
-import Recipe from "../../../models/Recipe";
-import { verifyToken } from "../../../middleware/auth";
+import dbConnect from "../../lib/mongodb";
+import Recipe from "../../models/Recipe";
+import { verifyToken } from "../../middleware/auth";
 
 export default async function handler(req, res) {
   const { method } = req;
