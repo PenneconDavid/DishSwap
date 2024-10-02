86f38ade8a1a346fa55d5c29c564007817e44f27
David Sebold <d.seibold@icloud.com>
2024-10-02 10:30:47 -0700

    added GET and DELETE routes to recipe.js api file

--- pages/api/recipes.js
+++ pages/api/recipes.js
@@ -1,6 +1,6 @@
-import dbConnect from "../../lib/mongodb";
-import Recipe from "../../models/Recipe";
-import { verifyToken } from "../../middleware/auth"; // Import the middleware
+import dbConnect from "../../../lib/mongodb";
+import Recipe from "../../../models/Recipe";
+import { verifyToken } from "../../../middleware/auth";
 
 export default async function handler(req, res) {
   const { method } = req;
@@ -18,11 +18,52 @@ export default async function handler(req, res) {
       break;
 
     case "POST":
-      // Use the middleware to verify token before allowing access
       verifyToken(req, res, async () => {
         try {
-          const recipe = await Recipe.create(req.body);
+          const recipe = await Recipe.create({
+            ...req.body,
+            userId: req.user.id,
+          });
           res.status(201).json({ success: true, data: recipe });
+        } catch (error) {
+          res.status(400).json({ success: false });
+        }
+      });
+      break;
+
+    case "PUT": // Update a recipe
+      verifyToken(req, res, async () => {
+        try {
+          const { id } = req.query;
+          const recipe = await Recipe.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true,
+          });
+          if (!recipe) {
+            return res
+              .status(400)
+              .json({ success: false, message: "Recipe not found" });
+          }
+          res.status(200).json({ success: true, data: recipe });
+        } catch (error) {
+          res.status(400).json({ success: false });
+        }
+      });
+      break;
+
+    case "DELETE": // Delete a recipe
+      verifyToken(req, res, async () => {
+        try {
+          const { id } = req.query;
+          const recipe = await Recipe.findByIdAndDelete(id);
+          if (!recipe) {
+            return res
+              .status(400)
+              .json({ success: false, message: "Recipe not found" });
+          }
+          res
+            .status(200)
+            .json({ success: true, message: "Recipe deleted successfully" });
         } catch (error) {
           res.status(400).json({ success: false });
         }
