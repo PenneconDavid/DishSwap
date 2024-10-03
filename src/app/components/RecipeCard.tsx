d188b011473e8ca9c24bb189b74ca3f98de8512a
David Sebold <d.seibold@icloud.com>
2024-10-03 16:14:44 -0700

    updated RecipeCard.tsx to link to recipe page on click

--- src/app/components/RecipeCard.tsx
+++ src/app/components/RecipeCard.tsx
@@ -1,22 +1,28 @@
+import Link from "next/link";
+
 interface RecipeCardProps {
+  _id: string;
   title: string;
   description: string;
   imageUrl: string;
 }
 
 const RecipeCard: React.FC<RecipeCardProps> = ({
+  _id,
   title,
   description,
   imageUrl,
 }) => {
   return (
-    <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition duration-300 transform hover:-translate-y-1">
-      <img src={imageUrl} alt={title} className="w-full h-48 object-cover" />
-      <div className="p-4">
-        <h2 className="text-xl font-bold text-gray-800">{title}</h2>
-        <p className="text-gray-600 mt-2">{description}</p>
+    <Link href={`/recipe/${_id}`} className="block">
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        <img src={imageUrl} alt={title} className="w-full h-48 object-cover" />
+        <div className="p-4">
+          <h3 className="text-xl font-semibold mb-2">{title}</h3>
+          <p className="text-gray-600">{description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
