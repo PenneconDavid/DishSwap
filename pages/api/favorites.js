2c18a61b59106cfd8470a3cb097f52cde51492f3
David Sebold <d.seibold@icloud.com>
2024-10-13 18:38:58 -0700

    fixed favorites API file

--- pages/api/favorites.js
+++ pages/api/favorites.js
@@ -1,49 +1,57 @@
 import dbConnect from "../../lib/mongodb";
+import User from "../../models/User";
+import { verifyToken } from "../../middleware/auth";
 import {
   getUserByEmail,
   addFavoriteRecipe,
   removeFavoriteRecipe,
 } from "../../utils/userUtils";
-import { getSession } from "next-auth/client";
+import nextConnect from "next-connect";
 
-export default async function handler(req, res) {
+const handler = nextConnect();
+
+handler.use(verifyToken);
+
+handler.get(async (req, res) => {
   await dbConnect();
-
-  const session = await getSession({ req });
-
-  if (!session) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  const userEmail = session.user.email;
-
   try {
-    const user = await getUserByEmail(userEmail);
-
-    switch (req.method) {
-      case "GET":
-        // Fetch favorite recipes
-        const favorites = await User.findOne({ email: userEmail })
-          .populate("favorites")
-          .select("favorites");
-        return res.status(200).json(favorites);
-      case "POST":
-        // Add a recipe to favorites
-        const { recipeId } = req.body;
-        await addFavoriteRecipe(user, recipeId);
-        return res.status(200).json({ message: "Recipe added to favorites" });
-      case "DELETE":
-        // Remove a recipe from favorites
-        const { id } = req.query;
-        await removeFavoriteRecipe(user, id);
-        return res
-          .status(200)
-          .json({ message: "Recipe removed from favorites" });
-      default:
-        return res.status(405).json({ message: "Method not allowed" });
-    }
+    const userEmail = req.user.email;
+    const favorites = await User.findOne({ email: userEmail })
+      .populate("favorites")
+      .select("favorites");
+    return res.status(200).json(favorites);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong" });
+    return res.status(500).json({ message: "Something went wrong" });
   }
-}
+});
+
+handler.post(async (req, res) => {
+  await dbConnect();
+  try {
+    const userEmail = req.user.email;
+    const { recipeId } = req.body;
+    const user = await getUserByEmail(userEmail);
+    await addFavoriteRecipe(user, recipeId);
+    return res.status(200).json({ message: "Recipe added to favorites" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+handler.delete(async (req, res) => {
+  await dbConnect();
+  try {
+    const userEmail = req.user.email;
+    const { id } = req.query;
+    const user = await getUserByEmail(userEmail);
+    await removeFavoriteRecipe(user, id);
+    return res.status(200).json({ message: "Recipe removed from favorites" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+export default handler;
