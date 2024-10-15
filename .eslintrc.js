67a7bd987f5d58bf56907914f9ea08d413ddea56
David Sebold <d.seibold@icloud.com>
2024-10-15 02:52:34 -0700

    fixed more deployment errors 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ["next/core-web-vitals"],
   rules: {
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@next/next/no-img-element": "off", // Only if you want to disable this rule project-wide
   },
 };
