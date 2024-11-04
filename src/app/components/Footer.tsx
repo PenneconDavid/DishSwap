fec62bce389247b1be1bc19d122971e1c3c96ec8
David Sebold <d.seibold@icloud.com>
2024-11-03 22:32:58 -0800

    Footer optimization 2

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-yellow-500 to-pink-500 text-white">
+    <footer className="bg-[#f4ecdf] text-black">
       <div className="container px-6 py-8 mx-auto">
         {/* Logo and Navigation Section */}
         <div className="flex flex-col items-center text-center">
