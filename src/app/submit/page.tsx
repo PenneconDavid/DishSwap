7ff0eba4086719b0928bc55c8e5b9f6ac0e2fc54
David Sebold <d.seibold@icloud.com>
2024-11-05 20:31:59 -0800

    Recipe form page and component page optimization 1

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import { motion } from "framer-motion";
 
 const RecipeForm = dynamic(() => import("../components/RecipeForm"), {
   ssr: false,
@@ -8,10 +9,16 @@ const Footer = dynamic(() => import("../components/Footer"), { ssr: false });
 
 export default function SubmitRecipePage() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-cream dark:bg-gray-900">
       <Header />
-      <main className="flex-grow flex flex-col justify-center">
-        <RecipeForm />
+      <main className="flex-grow container mx-auto px-4 py-8 sm:py-12 max-w-7xl">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <RecipeForm />
+        </motion.div>
       </main>
       <Footer />
     </div>
